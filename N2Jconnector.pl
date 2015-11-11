cffcbe085b746fd84de3fac18d626bfa0cdc1545
Julien <julien.arlandis@gmail.com>
2015-11-11 20:36:50 +0100

    update

--- N2Jconnector.pl
+++ N2Jconnector.pl
@@ -1,43 +1,85 @@
 #! /usr/bin/perl
+=pod
+Copyright © 2013-2014 Julien Arlandis
+    @author : Julien Arlandis <julien.arlandis_at_gmail.com>
+    @Licence : http://www.gnu.org/licenses/agpl-3.0.txt
 
-use strict; use warnings; use LWP::UserAgent; use JSON;
+This file is part of PhpNemoServer.
 
-my $DOMAIN = 'test.domain';
+    PhpNemoServer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PhpNemoServer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with PhpNemoServer.  If not, see <http://www.gnu.org/licenses/>.
+=cut
+
+use strict; 
+use warnings; 
+use LWP::UserAgent; 
+use JSON; 
+use utf8;
+use Data::Dumper qw(Dumper);
+
+my $DOMAIN = 'testfeed.nemoweb.net';
 my $USER_AGENT	= "N2JConnector/0.1";
-my (@query, $res);
+my ($query, $res);
 
-@query = ("diffuse", {(
-		"Propose" => {(
-			"DataType" => "NetNews",
-			"DataID" => "xxxx"
-			)},
-		"From" => $DOMAIN
-		)}
-);
+$query = <<"EOF";
+[
+    "diffuse",
+    {
+        "Propose": [
+            {
+                "Data": {
+                    "DataType": "NetNews",
+                    "DataID": "xxxxxdfghdfgfdsgds"
+                }
+            }
+        ],
+        "From": "$DOMAIN"
+    }
+]
+EOF
 
-$res = execJNTP("devnews.nemoweb.net", \@query);
+$res = execJNTP("devnews.nemoweb.net", $query);
 
-if($res->{'code'} eq "500") {
-	@query = ("diffuse", {(
-			"Data" => {(
-				"DataType" => "NetNews",
-				"DataID" => "hhhhh",
-				"Body" => getArticle()
-				)},
-			"From" => $DOMAIN
-			)}
-	);
+if($res->{'code'} eq "iwant") {
+
+	$query = <<"EOF";
+	[
+	    "diffuse",
+	    {
+		"Data":
+		    {
+	               "DataType": "NetNews",
+	               "DataID": "xxxxxdfghdfgfdsgds",
+	               "Body": ""
+		    },
+		"From": "$DOMAIN"
+	    }
+	]
+EOF
+	$query = decode_json($query);
+	$query->[1]{'Data'}{'Body'} = getArticle();
+	$query = encode_json($query);
 }
 
-$res = execJNTP("devnews.nemoweb.net", \@query);
-print $res->{'code'}."\n".$res->{'body'}."\n";
-
+$res = execJNTP("devnews.nemoweb.net", $query);
 
 sub execJNTP {
 	my $req = HTTP::Request->new(POST => "http://".$_[0]."/jntp/");
 	$req->header('content-type' => 'application/json');
-	$req->content(encode_json($_[1]));
-	return decode_json(LWP::UserAgent->new( agent => $USER_AGENT)->request($req)->content);
+	$req->content($_[1]);
+	my $response = LWP::UserAgent->new( agent => $USER_AGENT)->request($req)->content;
+	print $response."\n";
+	return decode_json($response);
 }
 
 sub getArticle {
